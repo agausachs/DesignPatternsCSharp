9397b7f869bc2e43b6d818f06aa71577bc41fc8d
Abraham Gausachs <undoitpubli@gmail.com>
2021-02-05 18:25:40 +0100

    Delete DesignPatterns/Patterns/AbstractFactoryPattern directory

--- DesignPatterns/Patterns/AbstractFactoryPattern/IAbstractProductA.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DesignPatterns.Patterns.AbstractFactoryPattern
-{
-    // Cada producto distinto de una familia de productos debe tener una interfaz básica.
-    // Todas las variantes del producto deben implementar esta interfaz.
-    public interface IAbstractProductA
-    {
-        string UsefulFunctionA();
-    }
-}

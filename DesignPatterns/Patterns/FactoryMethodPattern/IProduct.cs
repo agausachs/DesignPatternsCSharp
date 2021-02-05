3118d289788baed946229c41bd84e9beb188577c
Abraham Gausachs <undoitpubli@gmail.com>
2021-02-05 18:25:57 +0100

    Delete DesignPatterns/Patterns/FactoryMethodPattern directory

--- DesignPatterns/Patterns/FactoryMethodPattern/IProduct.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DesignPatterns.Patterns.FactoryMethodPattern
-{
-    // La interface de Product declara las operaciones que todos los products concretos
-    // deben implementar.
-    public interface IProduct
-    {
-        string Operation();
-    }
-}
